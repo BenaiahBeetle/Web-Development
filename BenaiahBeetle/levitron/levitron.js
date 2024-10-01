ad46223a7be2359f3753fb170f38606db403407c
Randall Saylor <158760805+BenaiahBeetle@users.noreply.github.com>
2024-10-01 14:16:08 +0000

    add levitron.js and modify levitron.html

---
+++ BenaiahBeetle/levitron/levitron.js
@@ -0,0 +1,26 @@
+// JavaScript for Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  // Smooth scroll with offset for fixed header
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+      const headerOffset = document.querySelector('header').offsetHeight; // Get height of header
+      const elementPosition = target.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - headerOffset;
+  
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    });
+  });
