f95310dbbb5cf3f04eb2a9ccd60443f2b6c2213d
Randall Saylor <158760805+BenaiahBeetle@users.noreply.github.com>
2024-10-01 00:22:52 +0000

    modify all files

--- BenaiahBeetle/script.js
+++ BenaiahBeetle/script.js
@@ -1,15 +1,11 @@
+// JavaScript for Smooth Scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
       e.preventDefault();
   
-      const target = document.querySelector(this.getAttribute('href'));
-      const headerOffset = document.querySelector('header').offsetHeight; // Get height of header
-      const elementPosition = target.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - headerOffset;
-  
-      window.scrollBy({
-        top: offsetPosition,
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
         behavior: 'smooth'
-          });
-        });
       });
+    });
+  });
+  
