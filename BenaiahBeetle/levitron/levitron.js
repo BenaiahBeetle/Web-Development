8a6a80c0bbb86ad3a464a2749c774d1b8c18cfbe
Randall Saylor <158760805+BenaiahBeetle@users.noreply.github.com>
2024-11-06 02:44:03 +0000

    modify levitron files

--- BenaiahBeetle/levitron/levitron.js
+++ BenaiahBeetle/levitron/levitron.js
@@ -1,26 +1,27 @@
-// JavaScript for Smooth Scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  // Smooth scroll with offset for fixed header
+});
+// Smooth scroll with offset for fixed header
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      const target = document.querySelector(this.getAttribute('href'));
-      const headerOffset = document.querySelector('header').offsetHeight; // Get height of header
-      const elementPosition = target.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - headerOffset;
-  
-      window.scrollBy({
-        top: offsetPosition,
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    const headerOffset = document.querySelector('header').offsetHeight; // Get height of header
+    const elementPosition = target.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
     });
   });
+});
+
+
